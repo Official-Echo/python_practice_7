9a2f19847ae199217d3417e7dff221c38d04b68b
Official-Echo <o.zolotarov@ukma.edu.ua>
2025-03-26 18:25:15 +0200

    Fix bug in output

--- app/io/output.py
+++ app/io/output.py
@@ -21,5 +21,5 @@ def write_to_file_builtin(text, file_name="data/output.txt"):
         file_name : str
             File handle of the file to write to. Defaults to "data/output.txt"
         """
-    with open("data/output.txt", "w") as file:
+    with open(file_name, "w") as file:
         file.write(text)

8647e197c102f6dc93ac2ad21451b34bc2fff76a
Official-Echo <o.zolotarov@ukma.edu.ua>
2025-03-26 18:08:58 +0200

    Sanitize main.py

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ def main():
     -------
     None
     """
-    # Read from different sources
     console_text = read_from_console()
     builtin_text = read_from_file_builtin()
     pandas_text = read_from_file_pandas()
 
-    # Output to console
     print_to_console(console_text)
     print_to_console(builtin_text)
     print_to_console(pandas_text)
 
-    # Write console input to file
     write_to_file_builtin(console_text)
 
 

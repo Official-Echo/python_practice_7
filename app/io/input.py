6e772cd7d2484093595fd007689c8408515d3387
Official-Echo <o.zolotarov@ukma.edu.ua>
2025-03-26 17:28:38 +0200

    Document IO

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,34 @@
 def read_from_console():
+    """
+    Reads text input from the console provided by the user.
+
+    Returns
+    -------
+    str
+        The text entered by the user via the console.
+    """
     pass
 
 
 def read_from_file_builtin():
+    """
+    Reads content from a text file using Python's built-in file handling.
+
+    Returns
+    -------
+    str
+        The content of the file as a single string.
+    """
     pass
 
 
 def read_from_file_pandas():
+    """
+    Reads content from a CSV file using the pandas library.
+
+    Returns
+    -------
+    str
+        The content of the CSV file as a string representation of a pandas DataFrame.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,22 @@
-def print_to_console():
+def print_to_console(text):
+    """
+    Prints the provided text to the console.
+
+    Parameters
+    ----------
+    text : str
+        The text to be printed to the console.
+    """
     pass
 
 
-def write_to_file_builtin():
+def write_to_file_builtin(text):
+    """
+        Writes the provided text to a file using Python's built-in file handling.
+
+        Parameters
+        ----------
+        text : str
+            The text to be written to the file.
+        """
     pass
